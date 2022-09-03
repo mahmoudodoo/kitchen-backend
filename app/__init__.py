ba76752053174f2d17e4e46d7208dfe54314010d
mahmoudodoo <mahmoudodeh332@gmail.com>
2022-09-03 23:30:46 +0300

    Editing

--- app/__init__.py
+++ app/__init__.py
@@ -23,12 +23,3 @@ from app.views import home,kitchen_view,register_view,login_view,cook_view
 from app.models import admin_model,cook_model,kitchen_model
 from app.api import admin_api,user_api,cook_api,kitchen_api
 
-@app.context_processor
-def instance_id():
-	instance_id = 'i-0e21c3c236adbedab'
-	try:
-		response = requests.get('http://169.254.169.254/latest/meta-data/i-0e21c3c236adbedab/', timeout=3)
-		instance_id = response.content.decode('utf-8')
-	except:
-		pass
-	return dict(instance_id=instance_id)
